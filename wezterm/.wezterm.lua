ad5f64ee2e99e1eed4c7edbdc9c3b042545202d9
Hrvoje Abraham <ahrvoje@gmail.com>
2024-08-13 22:39:17 +0200

    remove window title, add more keys

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,6 +3,7 @@ local act = wezterm.action
 
 local config = wezterm.config_builder()
 
+config.window_decorations = "RESIZE"
 config.font = wezterm.font 'Consolas'
 config.initial_rows = 36
 config.initial_cols = 101
@@ -13,22 +14,28 @@ config.colors = {
 }
 
 config.keys = {
-	{ key = 'LeftArrow',  mods = 'CTRL', action = act.ActivateTabRelative(-1) },
-	{ key = 'RightArrow', mods = 'CTRL', action = act.ActivateTabRelative(1) },
-	{ key = 't',          mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' },
-	{ key = 'w',          mods = 'CTRL', action = act.CloseCurrentTab{ confirm = true } },
-	{ key = 'RightArrow', mods = 'ALT',  action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
-	{ key = 'DownArrow',  mods = 'ALT',  action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
-	{ key = '1',          mods = 'ALT',  action = act.ActivateTab(0) },
-	{ key = '2',          mods = 'ALT',  action = act.ActivateTab(1) },
-	{ key = '3',          mods = 'ALT',  action = act.ActivateTab(2) },
-	{ key = '4',          mods = 'ALT',  action = act.ActivateTab(3) },
-	{ key = '5',          mods = 'ALT',  action = act.ActivateTab(4) },
-	{ key = '6',          mods = 'ALT',  action = act.ActivateTab(5) },
-	{ key = '7',          mods = 'ALT',  action = act.ActivateTab(6) },
-	{ key = '8',          mods = 'ALT',  action = act.ActivateTab(7) },
-	{ key = '9',          mods = 'ALT',  action = act.ActivateTab(8) },
-	{ key = 'Enter',      mods = 'ALT',  action = wezterm.action.ShowLauncher },
+	{ key = 't',          mods = 'CTRL',  action = act.SpawnTab 'CurrentPaneDomain' },
+	{ key = 'w',          mods = 'CTRL',  action = act.CloseCurrentTab{ confirm = true } },
+	{ key = 'LeftArrow',  mods = 'CTRL',  action = act.ActivateTabRelative(-1) },
+	{ key = 'RightArrow', mods = 'CTRL',  action = act.ActivateTabRelative(1) },
+	{ key = 'RightArrow', mods = 'ALT',   action = act.SplitHorizontal{ domain =  'CurrentPaneDomain' } },
+	{ key = 'DownArrow',  mods = 'ALT',   action = act.SplitVertical{ domain =  'CurrentPaneDomain' } },
+	{ key = 'LeftArrow',  mods = 'SHIFT', action = act.ActivatePaneDirection 'Left', },
+    { key = 'RightArrow', mods = 'SHIFT', action = act.ActivatePaneDirection 'Right', },
+	{ key = 'UpArrow',    mods = 'SHIFT', action = act.ActivatePaneDirection 'Up', },
+    { key = 'DownArrow',  mods = 'SHIFT', action = act.ActivatePaneDirection 'Down', },
+	{ key = '1',          mods = 'ALT',   action = act.ActivateTab(0) },
+	{ key = '2',          mods = 'ALT',   action = act.ActivateTab(1) },
+	{ key = '3',          mods = 'ALT',   action = act.ActivateTab(2) },
+	{ key = '4',          mods = 'ALT',   action = act.ActivateTab(3) },
+	{ key = '5',          mods = 'ALT',   action = act.ActivateTab(4) },
+	{ key = '6',          mods = 'ALT',   action = act.ActivateTab(5) },
+	{ key = '7',          mods = 'ALT',   action = act.ActivateTab(6) },
+	{ key = '8',          mods = 'ALT',   action = act.ActivateTab(7) },
+	{ key = '9',          mods = 'ALT',   action = act.ActivateTab(8) },
+	{ key = 'Enter',      mods = 'ALT',   action = act.ShowLauncher },
+	{ key = 'Enter',      mods = 'CTRL',  action = act.ShowTabNavigator },
+	{ key = 'l',          mods = 'CTRL',  action = act.ShowDebugOverlay },
 }
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
@@ -52,6 +59,5 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 end
 
 config.launch_menu = launch_menu
-
 
 return config
