2aa187e9370c231417be94766c120b5b0b9b7226
Hrvoje Abraham <ahrvoje@gmail.com>
2026-02-23 00:12:46 +0100

    nvim improve flash config

--- nvim/init.lua
+++ nvim/init.lua
@@ -561,11 +561,14 @@ vim.schedule(function()
     { "s", desc = "Flash" },
     { "S", desc = "Flash Treesitter" },
     { "r", mode = "o", desc = "Remote Flash" },
+    { "R", mode = "o", desc = "Treesitter Search" },
   }, function()
     require("flash").setup({})
     vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
     vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
     vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+    vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+    vim.keymap.set("c", "<C-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
   end)
 
   -- Vim-visual-multi (keys)
